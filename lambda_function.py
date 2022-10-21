65d779afc36c62b78e9b1f2844f1b3cf6f5e95b3
Louis Tomos Evans <LouisTomosEvans@protonmail.com>
2022-10-21 02:36:42 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,18 +2,54 @@ import sys
 import json
 from instagrapi import Client
 from instagrapi.types import StoryMention, StoryMedia, StoryLink, StoryHashtag
+import boto3
 
 def Instagram_Get_User_Info(SEARCH_USERNAME, cl):
-    # Use a breakpoint in the code line below to debug your script.
-    user = cl.user_info_by_username(SEARCH_USERNAME)
+    try:
+        user = cl.user_info_by_username(SEARCH_USERNAME)
+    except:
+        data = client.put_item(
+        TableName='long-poll',
+        Item={
+            'id': {
+                'S': retryID
+            },
+            'response': {
+                'S': "Error: Couldn't get user info!"
+            }
+        }
+        )
     if(user.is_private == False):
         return user.pk
-    else:
-        sys.exit("Error: User is Private!")
+    else{
+        data = client.put_item(
+        TableName='long-poll',
+        Item={
+            'id': {
+                'S': retryID
+            },
+            'response': {
+                'S': 'Error: User is private!'
+            }
+        }
+        )
+    }
 
 def Instagram_Get_User_Media(USER_ID, cl):
-    # Use a breakpoint in the code line below to debug your script.
-    medias = cl.user_medias_v1(USER_ID)
+    try:
+        medias = cl.user_medias_v1(USER_ID)
+    except:
+        data = client.put_item(
+        TableName='long-poll',
+        Item={
+            'id': {
+                'S': retryID
+            },
+            'response': {
+                'S': "Error: Couldn't get user's medias!"
+            }
+        }
+        )
     return medias
 
 
@@ -26,12 +62,28 @@ def lambda_handler(event, context):
 
     ## Example Use Multi-Proxy
 
+    ## Create dynamoDB
+    client = boto3.client('dynamodb')
+
+    ## Login
     cl = Client()
     cl.login(IG_Username, IG_Password)
+
+    ## Get Data
     UserID = Instagram_Get_User_Info(Search_Username, cl)
     UserMedia = Instagram_Get_User_Media(UserID, cl)
     mediaList = []
     for media in UserMedia:
         mediaList.append(media.__dict__)
-
-    return json.dumps(mediaList, sort_keys=True, default=str)
+    
+    data = client.put_item(
+    TableName='long-poll',
+    Item={
+        'id': {
+            'S': retryID
+        },
+        'response': {
+            'S': json.dumps(mediaList, sort_keys=True, default=str)
+        }
+    }
+    )
