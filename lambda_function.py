f04aa5e190c7b673f8ec6b47dafdb3bbce8405c5
Louis Tomos Evans <LouisTomosEvans@protonmail.com>
2022-11-01 00:12:07 +0000

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -76,7 +76,7 @@ def get_user():
     if(data['Item']['Error']['S'] != ""):
         return get_user()
     elif(data['Item']['date']['S'] != ""):
-        datetimeObj = datetime.strptime(data['Item']['date']['S'], '%Y-%m-%d %H:%M:%S')
+        datetimeObj = datetime.strptime(data['Item']['date']['S'], '%Y-%m-%d %H:%M:%S.%f')
         if(datetimeObj < datetime.now()):
             # delete error and date
             item = data['Item']
